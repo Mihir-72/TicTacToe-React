0b020092cb1e2a408ae40f423cc6d331c919a0b0
Mihir-72 <144117994+Mihir-72@users.noreply.github.com>
2024-02-04 03:43:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './Component/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+   <div>
+    <TicTacToe/>
+   </div>
+  );
+}
+
+export default App;
